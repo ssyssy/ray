24744d8342e030452cb297107273abec09e8a7d5
Siyuan Sheng <ssyssy@umich.edu>
2024-02-08 14:22:13 -0800

    Fix dataset

--- release/air_tests/air_benchmarks/workloads/xgboost_benchmark.py
+++ release/air_tests/air_benchmarks/workloads/xgboost_benchmark.py
@@ -29,7 +29,7 @@ _EXPERIMENT_PARAMS = {
         "cpus_per_worker": 1,
     },
     "10G": {
-        "data": " s3://air-example-data-2/10G-xgboost-data.parquet/",
+        "data": " s3://siyuan-overmind-bucket/10G-xgboost-data/",
         "num_workers": 1,
         "cpus_per_worker": 12,
     },
