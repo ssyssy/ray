2fb8c8d634904d46b3ba0c8316b858af57c5f5c4
Siyuan Sheng <ssyssy@umich.edu>
2024-02-12 17:39:41 -0800

    Fix page size

--- release/air_tests/air_benchmarks/workloads/xgboost_benchmark.py
+++ release/air_tests/air_benchmarks/workloads/xgboost_benchmark.py
@@ -92,7 +92,7 @@ def setup_alluxio(args):
     if args.alluxio_worker_hosts:
         alluxio_kwargs['worker_hosts'] = args.alluxio_worker_hosts
 
-    alluxio_kwargs['options'] = {"alluxio.worker.page.store.page.size": "100MB"}
+    alluxio_kwargs['options'] = {"alluxio.worker.page.store.page.size": "20MB"}
     if not alluxio_kwargs:
         raise ValueError("Either etcd_host or worker_hosts should be provided.")
 
